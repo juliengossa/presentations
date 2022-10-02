b63b1aa9516788074ee682c6573529408dda997a
juliengossa <julien.gossa@gmail.com>
2022-10-02 13:19:27 +0200

    modification minu=eure du theme

--- apd-dap/apddap.R
+++ apd-dap/apddap.R
@@ -3,8 +3,10 @@ rmarkdown::render("apddap.Rmd")
 
 staplr::split_pdf(input_filepath = "programme.pdf", output_directory = ".", prefix = "programme_p")
 
-staplr::select_pages(98:99,input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
-staplr::select_pages(295:308,input_filepath = "tdbesr-rapport.pdf", output_filepath = "classement.pdf")
+staplr::select_pages(99:100, input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
+staplr::rotate_pdf(page_rotation = 270, input_filepath = "unistra.pdf", output_filepath = "unistra.pdf")
+staplr::select_pages(296:309, input_filepath = "tdbesr-rapport.pdf", output_filepath = "classement.pdf")
+staplr::rotate_pdf(page_rotation = 270, input_filepath = "classement.pdf", output_filepath = "classement.pdf")
 
 staplr::staple_pdf(input_files = c("programme_p0001.pdf", 
                                    "apddap.pdf", 
