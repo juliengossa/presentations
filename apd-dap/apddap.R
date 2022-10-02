ada7699f4d852764adeccf5fd22963c2112a2849
juliengossa <julien.gossa@gmail.com>
2022-10-02 15:11:06 +0200

    Maj des données

--- apd-dap/apddap.R
+++ apd-dap/apddap.R
@@ -3,7 +3,7 @@ rmarkdown::render("apddap.Rmd")
 
 staplr::split_pdf(input_filepath = "programme.pdf", output_directory = ".", prefix = "programme_p")
 
-staplr::select_pages(99:100, input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
+staplr::select_pages(139:140, input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
 staplr::rotate_pdf(page_rotation = 270, input_filepath = "unistra.pdf", output_filepath = "unistra.pdf")
 staplr::select_pages(296:309, input_filepath = "tdbesr-rapport.pdf", output_filepath = "classement.pdf")
 staplr::rotate_pdf(page_rotation = 270, input_filepath = "classement.pdf", output_filepath = "classement.pdf")
@@ -14,3 +14,4 @@ staplr::staple_pdf(input_files = c("programme_p0001.pdf",
                                    "classement.pdf",
                                    "programme_p0002.pdf"), 
                    output_filepath = "apddap-print.pdf")
+
