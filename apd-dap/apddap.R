eaef5d91afc3665bbf016bbfe46942eb92b60b71
juliengossa <julien.gossa@gmail.com>
2022-10-03 23:11:08 +0200

    Bon nombre de pages

--- apd-dap/apddap.R
+++ apd-dap/apddap.R
@@ -3,15 +3,16 @@ rmarkdown::render("apddap.Rmd")
 
 staplr::split_pdf(input_filepath = "programme.pdf", output_directory = ".", prefix = "programme_p")
 
-staplr::select_pages(139:140, input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
-staplr::rotate_pdf(page_rotation = 270, input_filepath = "unistra.pdf", output_filepath = "unistra.pdf")
-staplr::select_pages(296:309, input_filepath = "tdbesr-rapport.pdf", output_filepath = "classement.pdf")
-staplr::rotate_pdf(page_rotation = 270, input_filepath = "classement.pdf", output_filepath = "classement.pdf")
+#staplr::select_pages(139:140, input_filepath = "tdbesr-rapport.pdf", output_filepath = "unistra.pdf")
+#staplr::rotate_pdf(page_rotation = 270, input_filepath = "unistra.pdf", output_filepath = "unistra.pdf")
+#staplr::select_pages(296:309, input_filepath = "tdbesr-rapport.pdf", output_filepath = "classement.pdf")
+#staplr::rotate_pdf(page_rotation = 270, input_filepath = "classement.pdf", output_filepath = "classement.pdf")
+
+staplr::rotate_pdf(page_rotation = 270, input_filepath = "apddap-extra.pdf", output_filepath = "apddap-extra-rot.pdf")
 
 staplr::staple_pdf(input_files = c("programme_p0001.pdf", 
                                    "apddap.pdf", 
-                                   "unistra.pdf",
-                                   "classement.pdf",
+                                   "apddap-extra-rot.pdf",
                                    "programme_p0002.pdf"), 
                    output_filepath = "apddap-print.pdf")
 
